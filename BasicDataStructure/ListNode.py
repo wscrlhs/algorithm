a9ad4a72ac7c280d33bbfa75fa51afde5953a47b
wscrlhs <wscrlhs@gmail.com>
2018-02-04 20:57:07 +0800

    单向链表

---
+++ BasicDataStructure/ListNode.py
@@ -0,0 +1,14 @@
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+    # in python next is a reversed world
+    def reverse(self, head):
+        prev = None
+        while head:
+            temp = head.next
+            head.next = prev
+            prev = head
+            head = temp
+        return prev
