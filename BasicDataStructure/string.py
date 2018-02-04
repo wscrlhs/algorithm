f2e43dceef158337a3e31d194ebab9b1bc0cb119
wscrlhs <wscrlhs@gmail.com>
2018-02-04 20:03:36 +0800

    string

---
+++ BasicDataStructure/string.py
@@ -0,0 +1,17 @@
+
+s1 = str()
+# in python `''` or `""` is the same
+s2 = "shaunwei" # 'shaunwei'
+s2len = len(s2)
+# last 3 chars
+s2[-3:] # wei
+s2[5:8] # wei
+s3 = s2[:5] # shaun
+s3 += 'wei' # return 'shaunwei'
+# list in python is same as ArrayList in java
+s2list = list(s3)
+# string at index 4
+s2[4] # 'n'
+# find index at first
+s2.index('w')  # return 5, if not found, throw ValueError
+s2.find('w') # return 5, if not found, return -1
