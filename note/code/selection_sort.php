e8d48f12cd810ae6372932c757dcbd297df74d83
wscrlhs <wscrlhs@gmail.com>
2018-03-28 00:43:20 +0800

    选择排序

---
+++ note/code/selection_sort.php
@@ -0,0 +1,28 @@
+<?php
+
+function findSmallest($arr)
+{
+    $smallest = $arr[0];
+    $smallestIndex = 0;
+    $length = count($arr);
+    for ($i = 0; $i < $length; $i++) {
+        if ($arr[$i] < $smallest) {
+            $smallest = $arr[$i];
+            $smallestIndex = $i;
+        }
+    }
+    return $smallestIndex;
+}
+
+function selectionSort($arr)
+{
+    $newArr = [];
+    $length = count($arr);
+    for ($i = 0; $i < $length; $i++) {
+        $smallest = findSmallest($arr);
+        array_push($newArr, array_splice($arr, $smallest, 1)[0]);
+    }
+    return $newArr;
+}
+
+var_dump(selectionSort([5, 3, 6, 2, 10]));
