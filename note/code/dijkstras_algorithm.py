98981b3904c0a9fb05d1026c20e744573c6638ee
wscrlhs <wscrlhs@gmail.com>
2018-04-06 23:25:35 +0800

    fixing

--- note/code/dijkstras_algorithm.py
+++ note/code/dijkstras_algorithm.py
@@ -43,15 +43,15 @@ def find_lowest_cost_node(costs):
 node = find_lowest_cost_node(costs)
 while node is not None:
     cost = costs[node]
-neighbors = graph[node]
-for n in neighbors.keys():
-    new_cost = cost + neighbors[n]
-if costs[n] > new_cost:
-    costs[n] = new_cost
-parents[n] = node
+    neighbors = graph[node]
+    for n in neighbors.keys():
+        new_cost = cost + neighbors[n]
+        if costs[n] > new_cost:
+            costs[n] = new_cost
+            parents[n] = node
 
-processed.append(node)
-node = find_lowest_cost_node(costs)
+    processed.append(node)
+    node = find_lowest_cost_node(costs)
 
 print('cost from the start to each node:')
 print(costs)
